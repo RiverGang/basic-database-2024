d48e9a5711e86fa4951a6532742f9663d8b4f061
RiverGang <sory304@naver.com>
2024-03-28 17:26:52 +0900

    수정

--- day01/sql003_샘플실행.sql
+++ day01/sql003_샘플실행.sql
@@ -13,5 +13,20 @@ SQL의 특징
 - 대소문자 구분없음 but, 키워드(파란색)는 대문자로 사용
 - 세미콜론(;) 필수 아님. but, 중요한 상황에서 사용
 */
-d
-USE basic2024;
+
+-- 김연아 고객의 전화번호를 찾으시오
+-- (*) => All 이라고 호칭함
+
+-- 1 step
+SELECT *
+  FROM Customer;
+
+-- 2 step
+SELECT *
+  FROM Customer
+ WHERE [name] = '김연아'; - [] name이 키워드가 아님을 명시
+
+-- 3 step
+SELECT phone
+  FROM Customer
+ WHERE [name] = '김연아';
