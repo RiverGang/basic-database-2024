ee2af7c53520afc1fa38055d32609a35247e25aa
RiverGang <sory304@naver.com>
2024-04-09 13:37:46 +0900

    sql테스트완료

--- day09/이성희.sql
+++ day09/이성희.sql
@@ -9,6 +9,7 @@ SELECT Email as email
   ORDER BY Addr DESC;
 
 
+
  -- 2번
  -- 함수를 사용하여 아래와 같은 결과가 나오도록 쿼리를 작성하시오.(전채 행의 개수는 59개입니다)
  
@@ -18,3 +19,45 @@ SELECT Names as '도서명'
 	 , Price as '정가'
   FROM bookstbl
 ORDER BY Price DESC;
+
+
+
+-- 3번
+-- 책을 한번도 빌린적이 없는 회원 출력 쿼리문
+SELECT m.Names AS '회원명'
+	 , m.Levels AS '회원등급'
+	 , m.Addr AS '회원주소'
+	 , r.rentalDate AS '대여일'
+  FROM rentaltbl AS r 
+  RIGHT OUTER JOIN membertbl AS m
+    ON r.memberIdx = m.memberIdx
+ WHERE r.memberIdx IS NULL
+   AND m.Levels = 'A'
+ 
+
+
+-- 4번 
+-- 책 장르별 총합계금액
+SELECT d.Names AS '책 장르'
+	 , p.총합계금액
+  FROM divtbl AS d
+	 , (SELECT b.Division
+		     , FORMAT(SUM(Price),'#,#') + ' 원' As 총합계금액
+		  FROM bookstbl AS b
+	  GROUP BY b.Division) as p
+ WHERE d.Division = p.Division
+
+
+
+
+-- 5번
+SELECT ISNULL(d.Names, '--합계--') AS '책 장르'
+	 , p.권수
+	 , p.총합계금액
+  FROM divtbl AS d
+  RIGHT OUTER JOIN (SELECT b.Division
+					     , COUNT(Division) AS '권수'
+						 , FORMAT(SUM(Price),'#,#') + ' 원' As 총합계금액
+					  FROM bookstbl AS b
+					  GROUP BY ROLLUP(b.Division)) as p
+	ON d.Division = p.Division
